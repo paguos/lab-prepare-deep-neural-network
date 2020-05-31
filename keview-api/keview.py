3f58eebbeb2cbb85c1ee1915eb14c31d4d688328
Philipp Pirlet <pirlet@campus.tu-berlin.de>
2020-05-31 19:10:55 +0200

    fix(view-api): add Convolution get_output

--- keview-api/keview.py
+++ keview-api/keview.py
@@ -1,8 +1,7 @@
-from tensorflow import keras
+from tensorflow.python import keras
 import numpy as np
 from keras import backend as K
 from abc import ABC, abstractmethod
-
 
 class Layer(ABC):
     def __init__(self, layer: keras.layers.Layer):
@@ -20,8 +19,8 @@ class Layer(ABC):
     def get_keras_layer(self):
         return self.__keras_layer
 
-    #@abstractmethod
-    def _set_output(self,output):
+    # @abstractmethod
+    def _set_output(self, output):
         pass
 
 
@@ -31,7 +30,7 @@ class DenseLayer(Layer):
 
         weights = layer.get_weights()
         weights[0] = np.einsum('kl->lk', weights[0])
-        self.__neurons = [Neuron(x, y) for x,y in zip(weights[0], weights[1])]
+        self.__neurons = [Neuron(x, y) for x, y in zip(weights[0], weights[1])]
         self.__activation_function = layer.get_config()["activation"]
 
     def get_neurons(self):
@@ -40,36 +39,54 @@ class DenseLayer(Layer):
     def get_activation_function(self):
         return self.__activation_function
 
-    def _set_output(self,layer_output):
-       for neuron,neuron_output in zip(self.__neurons,layer_output[0]):
+    def _set_output(self, layer_output):
+        for neuron, neuron_output in zip(self.__neurons, layer_output[0]):
             neuron._set_output(neuron_output);
+
+
 class ConvolutionLayer(Layer):
     def __init__(self, layer: keras.layers.Dense):
         Layer.__init__(self, layer)
 
         weights = layer.get_weights()
         weights[0] = np.einsum('klij->jikl', layer.get_weights()[0])
-        self.__featuremaps = [FeatureMap(x, y) for x,y in zip(weights[0], weights[1])]
+        self.__featuremaps = [FeatureMap(x, y) for x, y in zip(weights[0], weights[1])]
 
     def get_featuremaps(self):
         return self.__featuremaps
+
+    def _set_output(self, layer_output):
+        layer_output = np.einsum('abc->cab', np.reshape(layer_output, np.shape(layer_output)[1:]))
+        for featuremap, featuremap_output in zip(self.__featuremaps, layer_output):
+            #print(np.shape(featuremap_output))
+            featuremap._set_output(featuremap_output)
 
 
 class FlattenLayer(Layer):
     def __init__(self, layer: keras.layers.Flatten):
         Layer.__init__(self, layer)
 
+    def _set_output(self, output):
+        pass
+
 
 class FeatureMap:
     def __init__(self, weights: [], bias: np.float):
         self.__bias = bias
         self.__weights = weights
+        self.__output = None
 
     def get_weights(self):
         return self.__weights
 
     def get_bias(self):
         return self.__bias
+
+    def _set_output(self, output):
+        self.__output = output
+
+    def get_output(self):
+        return self.__output
 
 
 class Neuron:
@@ -77,13 +94,15 @@ class Neuron:
         self.__weights = weights
         self.__bias = bias
         self.__output = None
+
     def get_weights(self):
         return self.__weights
 
     def get_bias(self):
         return self.__bias
-    def _set_output(self,output):
-        self.__output=output
+
+    def _set_output(self, output):
+        self.__output = output
 
     def get_output(self):
         return self.__output
@@ -94,7 +113,7 @@ class Model:
     def __init__(self, model: keras.Model):
         self.__model = model
         self.__layers = []
-        self.__run_function=K.function([self.__model.input],[layer.output for layer in self.__model.layers])
+        self.__run_function = K.function([self.__model.input], [layer.output for layer in self.__model.layers])
         for layer in model.layers:
             self.__layers.append(self.__create_layer(layer))
 
@@ -104,10 +123,11 @@ class Model:
     def get_layers(self) -> [Layer]:
         return self.__layers
 
-    def run(self,single_element):
-        outputs=self.__run_function(np.asarray([single_element]))
-        for layer, layer_output in zip(self.__layers,outputs):
+    def run(self, single_element):
+        outputs = self.__run_function(np.asarray([single_element]))
+        for layer, layer_output in zip(self.__layers, outputs):
             layer._set_output(layer_output)
+
     def __create_layer(self, layer):
         if isinstance(layer, keras.layers.Dense):
             return DenseLayer(layer)
@@ -115,3 +135,5 @@ class Model:
             return FlattenLayer(layer)
         if isinstance(layer, keras.layers.Conv2D):
             return ConvolutionLayer(layer)
+        else:
+            return Layer(layer)
--- keview-api/test.py
+++ keview-api/test.py
@@ -1,27 +1,22 @@
-from tensorflow import keras
+from tensorflow.python import keras
 import keview
 import numpy as np
+import matplotlib.pyplot as plt
 from keras import backend as K
-test = keras.Sequential()
-test.add(keras.layers.Conv2D(filters=8, kernel_size=(5, 5), input_shape=(28, 28, 3)))
-test.add(keras.layers.Flatten())
-test.add(keras.layers.Dense(5,activation="sigmoid"))
-test.add(keras.layers.Dense(6,activation="sigmoid"))
+model=keras.models.load_model("model.h5")
+data = keras.datasets.mnist
+(train_images_temp, train_labels), (test_images_temp, test_labels) = data.load_data()
+train_images = train_images_temp / 255.0
+print(np.shape(train_images))
+test_images = test_images_temp / 255.0
+train_images = train_images.reshape(60000,28,28,1)
+test_images = test_images.reshape(10000,28,28,1)
 
-print(test.summary())
-
-
-t = keview.Model(test)
+print(model.summary())
+t = keview.Model(model)
 element=np.zeros((28,28,3),np.float32)
-print(test(np.asarray([element])))
-get_output = K.function([test.layers[0].input],
-                        [test.layers[0].output])
-[conv_outputs] = get_output(np.asarray([element]))
-print(id(test))
-t.run(element)
-for i in range(len(t.get_layers())):
-    print("layer ", i, " input shape: ", t.get_layers()[i].get_input_shape())
-    print("layer ", i, " output shape: ", t.get_layers()[i].get_output_shape())
-    print("")
-    if(isinstance(t.get_layers()[i],keview.DenseLayer)):
-        print(t.get_layers()[i].get_neurons()[0].get_output())
+#plt.imshow(test_images_temp[0])
+t.run(test_images[0])
+
+plt.imshow(t.get_layers()[3].get_featuremaps()[-1].get_output(),cmap="binary")
+plt.show()
