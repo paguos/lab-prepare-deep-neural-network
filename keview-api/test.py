9ac45243b2ff15d2d87e0cc0805c15ab439901a0
philipp <pirlet@campus.tu-berlin.de>
2020-07-06 19:24:53 +0200

    fix(model.h5):upload trained model test.h5

--- keview-api/test.py
+++ keview-api/test.py
@@ -3,7 +3,7 @@ import keview
 import numpy as np
 import matplotlib.pyplot as plt
 from keras import backend as K
-model=keras.models.load_model("model.h5")
+model=keras.models.load_model("test.h5")
 data = keras.datasets.mnist
 (train_images_temp, train_labels), (test_images_temp, test_labels) = data.load_data()
 train_images = train_images_temp / 255.0
@@ -11,12 +11,16 @@ print(np.shape(train_images))
 test_images = test_images_temp / 255.0
 train_images = train_images.reshape(60000,28,28,1)
 test_images = test_images.reshape(10000,28,28,1)
+image=23
 
 print(model.summary())
 t = keview.Model(model)
 element=np.zeros((28,28,3),np.float32)
-#plt.imshow(test_images_temp[0])
-t.run(test_images[0])
-
+plt.imshow(test_images_temp[image],cmap='binary')
+t.run(test_images[image])
+plt.show()
 plt.imshow(t.get_layers()[3].get_featuremaps()[-1].get_output(),cmap="binary")
+l=t.get_layers()[-1].get_neurons();
+for i in range(len(l)):
+    print(i,": ",l[i].get_output())
 plt.show()
