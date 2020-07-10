2676e2304b664d235246cf9bf7d7e03068386df7
Pablo Osinaga <pabloosinaga@MacBook-Pro.local>
2020-07-10 12:43:03 +0200

    refactor(keview-api): test

--- keview-api/test.py
+++ keview-api/test.py
@@ -1,26 +1,34 @@
-from tensorflow.python import keras
 import keview
 import numpy as np
 import matplotlib.pyplot as plt
-from keras import backend as K
-model=keras.models.load_model("test.h5")
+
+from tensorflow.python import keras
+from loguru import logger
+
+logger.info("Loading model ...")
+model = keras.models.load_model("test.h5")
+logger.info("Loading dataset ....")
 data = keras.datasets.mnist
 (train_images_temp, train_labels), (test_images_temp, test_labels) = data.load_data()
 train_images = train_images_temp / 255.0
-print(np.shape(train_images))
-test_images = test_images_temp / 255.0
-train_images = train_images.reshape(60000,28,28,1)
-test_images = test_images.reshape(10000,28,28,1)
-image=23
 
-print(model.summary())
+logger.info(f"Test Images Shape: {np.shape(train_images)}")
+test_images = test_images_temp / 255.0
+train_images = train_images.reshape(60000, 28, 28, 1)
+test_images = test_images.reshape(10000, 28, 28, 1)
+image = 23
+
+logger.info(f"Model Summary: {model.summary()}")
 t = keview.Model(model)
-element=np.zeros((28,28,3),np.float32)
-plt.imshow(test_images_temp[image],cmap='binary')
+element = np.zeros((28, 28, 3), np.float32)
+plt.imshow(test_images_temp[image], cmap='binary')
 t.run(test_images[image])
 plt.show()
-plt.imshow(t.get_layers()[3].get_featuremaps()[-1].get_output(),cmap="binary")
-l=t.get_layers()[-1].get_neurons();
-for i in range(len(l)):
-    print(i,": ",l[i].get_output())
+plt.imshow(t.get_layers()[3].get_featuremaps()[-1].get_output(), cmap="binary")
+neurons = t.get_layers()[-1].get_neurons()
+index = 0
+
+for n in neurons:
+    logger.info(f"{index}:{n.get_output()}")
+    index = index + 1
 plt.show()
