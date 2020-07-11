b0f4907f801bbdad5ac6d3e5f075d86f699fdad5
Pablo Osinaga <pabloosinaga@MacBook-Pro.fritz.box>
2020-07-11 16:52:30 +0200

    feat(keview-api): add exception handling

--- keview-api/keview/api.py
+++ keview-api/keview/api.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from loguru import logger
 
 from tensorflow.python import keras
 
-from keview.models import KerasModel
+from keview.models import KerasModel, Layer
 from keview.utils import NumpyEncoder
 
 
@@ -33,7 +33,7 @@ async def layers():
 
 @app.get("/keview/v1alpha/layers/{layer_id}")
 async def layer(layer_id):
-    layer = keras_model.get_layers()[int(layer_id)]
+    layer = fetch_layer(keras_model, layer_id)
     layer_data = layer.toJSON()
     layer_data["id"] = layer_id
     return layer_data
@@ -41,20 +41,40 @@ async def layer(layer_id):
 
 @app.get("/keview/v1alpha/layers/{layer_id}/components")
 async def components(layer_id):
-    layer = keras_model.get_layers()[int(layer_id)]
+    layer = fetch_layer(keras_model, layer_id)
     components = [c.toJSON() for c in layer.get_components()]
     return NumpyEncoder.encodeJSON(components)
 
 
 @app.get("/keview/v1alpha/layers/{layer_id}/components/{component_id}")
 async def component(layer_id, component_id):
-    layer = keras_model.get_layers()[int(layer_id)]
-    component = layer.get_components()[int(component_id)]
+    layer = fetch_layer(keras_model, layer_id)
+    component = fetch_component(layer, component_id)
     return NumpyEncoder.encodeJSON(component.toJSON())
 
 
 @app.get("/keview/v1alpha/layers/{layer_id}/outputs")
 async def outputs(layer_id):
-    layer = keras_model.get_layers()[int(layer_id)]
+    layer = fetch_layer(keras_model, layer_id)
     outputs = [c.toJSON()["output"] for c in layer.get_components()]
     return NumpyEncoder.encodeJSON(outputs)
+
+
+def fetch_layer(model: KerasModel, layer_index):
+    try:
+        return keras_model.get_layers()[int(layer_index)]
+    except IndexError:
+        logger.error(f"Index out of range: {layer_index}")
+        raise HTTPException(status_code=404, detail="Layer not found.")
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid layer id.")
+
+
+def fetch_component(layer: Layer, component_index):
+    try:
+        return layer.get_components()[int(component_index)]
+    except IndexError:
+        logger.error(f"Index out of range: {component_index}")
+        raise HTTPException(status_code=404, detail="Layer not found.")
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid component id.")
