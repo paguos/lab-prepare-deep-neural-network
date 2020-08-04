73d2a96fb5b3fbc582f2c18c730c66274e220a43
Pablo Osinaga <pabloosinaga@MacBook-Pro.fritz.box>
2020-08-05 01:00:56 +0200

    wip: api with display endpoint

--- keview-api/keview/api.py
+++ keview-api/keview/api.py
@@ -3,7 +3,6 @@ import os
 import shutil
 
 import matplotlib.pyplot as plt
-import numpy as np
 from PIL import Image
 from fastapi import FastAPI, HTTPException, Request
 from fastapi import File, UploadFile
@@ -15,59 +14,27 @@ from tensorflow.python import keras
 
 
 from keview.models import KerasModel, Layer
-from keview.utils import NumpyEncoder
+from keview.utils import MNIST, NumpyEncoder
 
-""" --------------- DELTE AFTER TESTING -------------------- """
-data = keras.datasets.mnist
-(train_images_temp, train_labels), (test_images_temp, test_labels) = data.load_data()
-train_images = train_images_temp / 255.0
+IMAGES_DIR = "assets/images"
 
-logger.info(f"Test Images Shape: {np.shape(train_images)}")
-test_images = test_images_temp / 255.0
-train_images = train_images.reshape(60000, 28, 28, 1)
-test_images = test_images.reshape(10000, 28, 28, 1)
-image = 23
-""" ----------------------------------- """
 
-logger.info("Loading model ...")
-model = keras.models.load_model("examples/test.h5")
+logger.info("Initalizing model ...")
+model = keras.models.load_model("assets/keview_model.h5")
 keras_model = KerasModel(model)
-logger.info("Loading model ... done!")
-
-keras_model.run(test_images[image])
+keras_model.run(MNIST.get_test_image())
+logger.info("Initalizing model ... done!")
 
 app = FastAPI()
 
-# https://fastapi.tiangolo.com/advanced/templates/
-app.mount("/static", StaticFiles(directory="../UI/static"), name="static")
-templates = Jinja2Templates(directory="../UI/templates")
-
-
-"""------------------------------------------------------------------------------------------------------------------------- """
-
-
-# https://stackoverflow.com/questions/185936/how-to-delete-the-contents-of-a-folder
-def clearFolder():
-    folder = '../UI/static/images/tmp/'
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+templates = Jinja2Templates(directory="assets/templates")
 
 
 def isConvLayer(layer_id, request):
     featuremap = keras_model.get_layers()[int(layer_id)].get_featuremaps()[-1]
     output = featuremap.get_output()
-    # plt.imshow(output, cmap="binary")
-    # plt.show()
-    plt.imsave('../UI/static/images/tmp/tmp_conv2d.png', output)
+    plt.imsave(f'{IMAGES_DIR}/conv2d.png', output)
     bias = featuremap.get_bias()
     weights = featuremap.get_weights()
 
@@ -75,15 +42,14 @@ def isConvLayer(layer_id, request):
                                       {"request": request, "bias": bias, "weights": weights, "output": output})
 
 
-
-
 def isBatchNormalizationLayer(layer_id, request):
-    batch_normalization_layer = keras_model.get_layers()[int(layer_id)].get_normalization_dimensions()
+    batch_normalization_layer = keras_model.get_layers(
+    )[int(layer_id)].get_normalization_dimensions()
     for i in range(len(batch_normalization_layer)):
         output = batch_normalization_layer[i].get_output()
         # plt.imshow(output, cmap="binary")
         # plt.show()
-        plt.imsave('../UI/static/images/tmp/tmp_batch_normalization_' + str(i) + '.png', output)
+        plt.imsave('/Users/pabloosinaga/Development/TU-Berlin/lab-prepare-deep-neural-network/UI/static/images/tmp/tmp_batch_normalization_' + str(i) + '.png', output)
 
         beta = batch_normalization_layer[i].get_beta()
         gamma = batch_normalization_layer[i].get_gamma()
@@ -93,7 +59,6 @@ def isBatchNormalizationLayer(layer_id, request):
     return templates.TemplateResponse("batch_normalization_layer.html",
                                       {"request": request, "beta": beta, "mean": mean, "gamma": gamma,
                                        "variance": variance, "output": output})
-
 
 
 def isDenseLayer(layer_id, request):
@@ -109,17 +74,17 @@ def isDenseLayer(layer_id, request):
     return templates.TemplateResponse("dense_layer.html", {"request": request, "neurons": neuron_list})
 
 
-
-
 def isMaxPoolingLayer(layer_id, request):
-    max_pooling_dim = keras_model.get_layers()[int(layer_id)].get_max_pooling_dimensions()
+    max_pooling_dim = keras_model.get_layers(
+    )[int(layer_id)].get_max_pooling_dimensions()
     for i in range(len(max_pooling_dim)):
         output = max_pooling_dim[i].get_output()
         #plt.imshow(output, cmap="binary")
         # plt.show()
-        plt.imsave('../UI/static/images/tmp/tmp_max_pooling_' + str(i) + '.png', output)
+        plt.imsave('/Users/pabloosinaga/Development/TU-Berlin/lab-prepare-deep-neural-network/UI/static/images/tmp/tmp_max_pooling_' + str(i) + '.png', output)
 
     return templates.TemplateResponse("max_pooling_layer.html", {"request": request, "output": output})
+
 
 """------------------------------------------------------------------------------------------------------------------------- """
 
@@ -128,7 +93,6 @@ def isMaxPoolingLayer(layer_id, request):
 async def visualization():
     # show the summary of model
     return model.summary()
-
 
 
 @app.get("/keview/v1alpha/layers")
@@ -148,13 +112,42 @@ async def layers():
 
 
 @app.get("/keview/v1alpha/layers/{layer_id}")
-async def layer(request: Request, layer_id: str):
+async def layer(layer_id):
+    layer = fetch_layer(keras_model, layer_id)
+    layer_data = layer.toJSON()
+    layer_data["id"] = layer_id
+    return layer_data
+
+
+@app.get("/keview/v1alpha/layers/{layer_id}/components")
+async def components(layer_id):
+    layer = fetch_layer(keras_model, layer_id)
+    components = [c.toJSON() for c in layer.get_components()]
+    return NumpyEncoder.encodeJSON(components)
+
+
+@app.get("/keview/v1alpha/layers/{layer_id}/components/{component_id}")
+async def component(layer_id, component_id):
+    layer = fetch_layer(keras_model, layer_id)
+    component = fetch_component(layer, component_id)
+    return NumpyEncoder.encodeJSON(component.toJSON())
+
+
+@app.get("/keview/v1alpha/layers/{layer_id}/outputs")
+async def outputs(layer_id):
+    layer = fetch_layer(keras_model, layer_id)
+    outputs = [c.toJSON()["output"] for c in layer.get_components()]
+    return NumpyEncoder.encodeJSON(outputs)
+
+
+@app.get("/keview/v1alpha/layers/{layer_id}/display")
+async def display_layer(request: Request, layer_id: str):
     layer = fetch_layer(keras_model, layer_id)
     layer_data = layer.toJSON()
     layer_data["id"] = layer_id
 
     logger.info("Start clearing folder...")
-    clearFolder()
+    # clearFolder()
     logger.info("done!")
 
     if layer_id == "0" or layer_id == "3":
@@ -179,27 +172,6 @@ async def layer(request: Request, layer_id: str):
         print("ERROR: Invalid Layer ID! Choose an ID in range [0, 8].")
 
     # return layer_data
-
-
-@app.get("/keview/v1alpha/layers/{layer_id}/components")
-async def components(layer_id):
-    layer = fetch_layer(keras_model, layer_id)
-    components = [c.toJSON() for c in layer.get_components()]
-    return NumpyEncoder.encodeJSON(components)
-
-
-@app.get("/keview/v1alpha/layers/{layer_id}/components/{component_id}")
-async def component(layer_id, component_id):
-    layer = fetch_layer(keras_model, layer_id)
-    component = fetch_component(layer, component_id)
-    return NumpyEncoder.encodeJSON(component.toJSON())
-
-
-@app.get("/keview/v1alpha/layers/{layer_id}/outputs")
-async def outputs(layer_id):
-    layer = fetch_layer(keras_model, layer_id)
-    outputs = [c.toJSON()["output"] for c in layer.get_components()]
-    return NumpyEncoder.encodeJSON(outputs)
 
 
 @app.post("/keview/v1alpha/test/")
--- keview-api/keview/utils.py
+++ keview-api/keview/utils.py
@@ -1,5 +1,8 @@
 import json
+import random
 import numpy as np
+
+from tensorflow.python import keras
 
 
 class NumpyEncoder(json.JSONEncoder):
@@ -14,3 +17,16 @@ class NumpyEncoder(json.JSONEncoder):
     def encodeJSON(dict: dict):
         dump = json.dumps(dict, cls=NumpyEncoder)
         return json.loads(dump)
+
+
+class MNIST:
+
+    @staticmethod
+    def get_test_image():
+        data = keras.datasets.mnist
+        (_, _), (test_images_temp, _) = data.load_data()
+        test_images = test_images_temp / 255.0
+        test_images = test_images.reshape(10000, 28, 28, 1)
+
+        image_index = random.randint(0, len(test_images))
+        return test_images[image_index]
